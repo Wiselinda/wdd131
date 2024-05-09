993782187aa7fd759f9bac1f12954fc7c274c1b9
Wiselinda <zid24001@byui.edu>
2024-05-09 20:16:44 +0300

    fixe

--- scripts/place.js
+++ scripts/place.js
@@ -1,4 +1,3 @@
-
 function calculateWindChill(temperature, windSpeed) {
     if (temperature <= 10 && windSpeed > 4.8) {
         const windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
